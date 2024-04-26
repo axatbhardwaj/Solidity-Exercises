834d7baa968cbac9885f7a3f3822ea9c8ada7f1f
axatbhardwaj <axatbhardwaj@outlook.com>
2024-04-26 15:40:28 +0530

    till storage

--- BasicBank/src/BasicBank.sol
+++ BasicBank/src/BasicBank.sol
@@ -1,11 +1,32 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
+error NoEtherSent();
+error NotEnoughBalance(uint256);
+
 contract BasicBank {
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
 
+    mapping (address => uint) balanceBook;
+
+    function addEther() external payable {
+        if(msg.value <= 0)
+        {
+            revert NoEtherSent();
+        }
+
+        balanceBook[msg.sender]=msg.value;
+
+    }
     /// @notice used to withdraw ether from the contract (No restriction on withdrawals)
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+        
+        if(balanceBook[msg.sender]<amount)
+        {
+            revert NotEnoughBalance(balanceBook[msg.sender]);
+        }
+        balanceBook[msg.sender]-=amount;
+        payable(msg.sender).transfer(amount);
+    }
 }
