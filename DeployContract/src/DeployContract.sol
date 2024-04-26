cf3c51c762020ec73599f2f40439e737172c49d7
axatbhardwaj <axatbhardwaj@outlook.com>
2024-04-27 02:14:40 +0530

    till deploy contract

--- DeployContract/src/DeployContract.sol
+++ DeployContract/src/DeployContract.sol
@@ -8,26 +8,19 @@ contract Deployer {
         2. Deploy the contract and return the address in `deployContract` function.
     */
 
-    function deployContract() public view returns (address) {
+    function deployContract() public returns (address) {
+        // your code here
+        EmptyContract newInstance = new EmptyContract();
+        address newAddress= address(newInstance);
 
-        bytes memory bytecode = type(EmptyContract).creationCode;
-        
-        bytes32 hash = keccak256((abi.encodePacked(bytes1(0xff),address(this),block.number,keccak256(bytecode))));
-        return address(uint160(uint(hash)));
-        
+        return newAddress;
     }
 }
 
 
 contract EmptyContract {
-
     address public owner;
     uint256 public foo;
-
-    constructor(address _owner, uint256 _foo) payable {
-        owner = _owner;
-        foo = _foo;
-    }
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
